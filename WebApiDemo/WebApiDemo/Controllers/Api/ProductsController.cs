8313f5e2d3febc76400ed8dc92a48e0d235ae841
Vaibhav <vaibhavkumar5219@gmail.com>
2022-04-28 09:24:17 +0530

    Initial Commit
    URL -https://localhost:44342/api/products/2
    
    https://localhost:44342/api/products

---
+++ WebApiDemo/WebApiDemo/Controllers/Api/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using WebApiDemo.Models;
+using System.Web.Http;
+
+
+namespace WebApiDemo.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]{
+         new Product { ID = 1, Name = "Chocolate", AddingDate =
+            DateTime.Parse(DateTime.Today.ToString()), Numbers = 30 },
+         new Product { ID = 2, Name = "Biscuits", AddingDate =
+            DateTime.Parse(DateTime.Today.ToString()), Numbers = 20 },
+         new Product { ID = 3, Name = "Biscuits", AddingDate =
+            DateTime.Parse(DateTime.Today.ToString()), Numbers = 20 },
+        };
+        public IEnumerable<Product> GetAllProduct()
+        {
+            return products;
+        }
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.ID == id);
+            
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ WebApiDemo/WebApiDemo/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiDemo.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime AddingDate { get; set; }
+        public int Numbers { get; set; }
+    }
+}
